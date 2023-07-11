08fc64a7eb91558eca32f1b94e91120fc27d8812
Abdul Moeed <abdulmoeedcj@gmail.com>
2023-07-11 15:37:33 +0500

    added env

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+require('dotenv').config({ path: '/.env' });
 const app = express();
+
+const port = process.env.PORT || 5001;
 
 app.use('/',(req,res)=>{
     res.send('server is runningss')
 })
 
-app.listen(5000 , ()=>{
+app.listen(port , ()=>{
     console.log("Listening at port: 5000" );
 })
 
